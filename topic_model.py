fea1564707ed92341ebc68ad600a7e31413c2a8d
adenletchworth <adenrletchworth@gmail.com>
2023-09-26 16:54:17 -0700

    feat: added LDA model and Coherence Model, Waiting for Data

--- topic_model.py
+++ topic_model.py
@@ -4,7 +4,7 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import WordPunctTokenizer
 import nltk
 from gensim.corpora import Dictionary
-from gensim.models import CoherenceModel
+from gensim.models import CoherenceModel, LdaModel
 
 df = pd.read_csv('Data/small_data.csv')
 
@@ -28,21 +28,35 @@ def process_text(text):
     lemmatized_words = [lz.lemmatize(word) for word in words]
 
     # lowercases and filters words
-    filtered_words = [word for word in lemmatized_words if word.lower() not in stop_words]
+    filtered_words = [word.lower() for word in lemmatized_words if word.lower() not in stop_words]
 
     processed_text = ' '.join(filtered_words)
 
     return processed_text
 
+# Add stop words to be filtered out
 def add_stop_words(words):
+
     stop_words.extend(words)
 
+# creates a list of lists of stop words
+reviews = [list(map(process_text, df.review_body))]
 
-reviews = list(map(process_text, df.review_body))
+# Create Dictionary
+id2word = Dictionary(reviews)
 
+# Create Corpus
+corpus = [id2word.doc2bow(text) for text in reviews]
 
+# Initialize LDA
+lda = LdaModel(corpus,5,id2word)
 
+# Initialize Coherence Model
+coherence_model_lda = CoherenceModel(model=lda, texts=reviews, dictionary=id2word,coherence='u_mass')
 
+# Get coherence score
+coherence_lda = coherence_model_lda.get_coherence()
 
+print('Coherence Score: ', coherence_lda)
 
-
+# NEED TO IMPLEMENT OPTIMAL NUMBER OF LABELS, NEED FINAL DATA
