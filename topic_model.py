bb4d10ca12b0f869adebd1346ea312a92813090c
adenletchworth <adenrletchworth@gmail.com>
2023-10-01 16:04:43 -0700

    feat: added tfidf for removing common words

--- topic_model.py
+++ topic_model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from nltk.corpus import stopwords
 from gensim.corpora import Dictionary
-from gensim.models import CoherenceModel, LdaModel
+from gensim.models import CoherenceModel, LdaModel, TfidfModel
 from gensim.models.phrases import Phrases, Phraser
 from gensim.utils import simple_preprocess
 import re
@@ -19,10 +19,8 @@ nlp = spacy.load('en_core_web_sm',disable=['parser','ner'])
 # Initialize Stop words
 stop_words = stopwords.words("english")
 
-# Define Hyperparameters
-ALPHA = 0.1
-BETA = 0.1
-NUM_TOPICS = 5
+# Define Hyperparameters 
+NUM_TOPICS = 10
 
 # Add stop words to be filtered out
 add_stop_words = lambda words: stop_words.extend(words)
@@ -47,23 +45,74 @@ def filter_tokens(texts):
 lemma = get_lemma(df.review_body)
 
 # Pass to spacy process function
-final_data=  filter_tokens(lemma)
+text_data=  filter_tokens(lemma)
         
-'''
-# Build the bigram and trigram models
-bigram = Phrases(tokens, min_count=5, threshold=10e-5) # higher threshold fewer phrases.
-trigram = Phrases(bigram[tokens], threshold=100)  
 
-# Faster way to get a sentence clubbed as a trigram/bigram
+
+bigram = Phrases(
+    text_data, 
+    min_count=5, 
+    threshold=50
+) 
+
+trigram = Phrases(
+    bigram[text_data], 
+    threshold=50
+)  
+
 bigram_mod = Phraser(bigram)
 trigram_mod = Phraser(trigram)
-'''
 
+def bigram_to_text(texts):
+    return [bigram[doc] for doc in texts]
+
+def trigram_to_text(texts):
+    return [trigram[bigram[doc]] for doc in texts]
+    
+data_bigrams = bigram_to_text(text_data)
+
+data_trigrams = trigram_to_text(data_bigrams)
+
+id2word= Dictionary(data_trigrams)
+
+corpus = [id2word.doc2bow(text) for text in data_trigrams]
+
+tfidf = TfidfModel(
+    corpus=corpus,
+    id2word=id2word
+)
+
+# Set threshold for 'valuable' words
+LOW = 0.03
+
+words = []
+
+missing_words = []
+
+for i in range(0,len(corpus)):
+    bow=corpus[i]
+    low_words = []
+
+    tfidf_ids = [id for id, value in tfidf[bow]]
+    bow_ids = [id for id, value in bow]
+
+    low_words = [id for id, value in tfidf[bow] if value < LOW]
+    drops = low_words+missing_words
+
+    for item in drops:
+        words.append(id2word[item])
+
+    missing_words = [id for id in bow_ids if id not in tfidf_ids]
+
+    new_bow = [b for b in bow if b[0] not in low_words and b[0] not in missing_words]
+    corpus[i]=  new_bow
+'''
 # Create Dictionary
-id2word = Dictionary(final_data)
+id2word = Dictionary(text_data)
 
 # Create Corpus
-corpus = [id2word.doc2bow(text) for text in final_data]
+corpus = [id2word.doc2bow(text) for text in text_data]
+'''
 
 # Initialize LDA
 lda = LdaModel(
@@ -80,7 +129,7 @@ lda = LdaModel(
 # Initialize Coherence Model
 coherence_model_lda = CoherenceModel(
     model=lda, 
-    texts=final_data, 
+    texts=text_data, 
     dictionary=id2word,
     coherence='u_mass'
 )
